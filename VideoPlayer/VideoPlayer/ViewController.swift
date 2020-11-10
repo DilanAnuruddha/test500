cd709310bb24865035093373cfade9cd13c41f2c
DilanAnuruddha <dilananuruddha23@yahoo.com>
2020-11-10 20:26:17 +0530

    Initial Commit

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Dilan Anuruddha on 11/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
